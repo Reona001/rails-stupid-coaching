3d5fd2d257c5aaf6c7dbc3502f6e5e8284a958f0
reona kasuya <reon.kasuya@gmail.com>
2022-01-04 23:53:01 +0900

    basic exercise completeted

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/question', to: "questions#ask"
+  get '/answer', to: "questions#answer"
 end
+
+
+# localhost:3000/answer?question=hello
+# localhost:3000/answer?question=what time is it?
